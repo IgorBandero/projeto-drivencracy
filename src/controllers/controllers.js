2e47bca15f28ad569b9f3bc60ad49f194e3d8df5
Igor Bandero <igorbandero@gmail.com>
2023-07-21 08:45:17 -0300

    feat: GET /poll/:id/choice implementado

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -79,10 +79,11 @@ export async function createChoice(req, res){
         const todayDate = dayjs();
 
         if (date.isBefore(todayDate)){
-            return res.status(403).send("Enquete já expirou!");
+            return res.status(403).sen
+            d("Enquete já expirou!");
         }
 
-        if (await db.collection("choices").findOne({title: newAlternative.title})){
+        if(await db.collection("choices").findOne({title: newAlternative.title, pollId: newAlternative.pollId})){
             return res.status(409).send("Alternativa já existe!");
         }
         else {
@@ -105,12 +106,29 @@ export async function createChoice(req, res){
     
 
 export async function getChoices(req, res){
-    try {
+
+    const {id} = req.params;
+    const poll = await db.collection("polls").findOne({_id: new ObjectId(id)});
+    console.log(poll);
+
+    if (poll){
+
+        const query = { pollId: id };
+        try {
+            const choicesList = await db.collection("choices").find({ pollId: new ObjectId(id) }).toArray();
+            console.log(choicesList);
+            res.send(choicesList);
         
-    } catch (error) {
-        res.status(500).send(error.message);
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    }
+    else {
+        return res.status(404).send("Enquete não encontrada!");
     }
 }
+
+// #####################################################################################
 
 export async function newVote(req, res){
     try {
