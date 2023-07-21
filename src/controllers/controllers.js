2089687cd043a6fe24e88b9e7a1eec89a3ab4b6c
Igor Bandero <igorbandero@gmail.com>
2023-07-21 00:04:12 -0300

    feat: GET /poll implementado

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,6 +1,8 @@
 import { db } from "../database/database.config.js";
 import dayjs from "dayjs";
 
+//#####################################################################################
+
 export async function createPoll(req, res){
     
     let { title, expireAt } = req.body;  
@@ -23,13 +25,19 @@ export async function createPoll(req, res){
     }
 }
 
+// #####################################################################################
+
 export async function getPoll(req, res){
+
     try {
-        
+        const pollsList = await db.collection("polls").find().toArray();
+        res.send(pollsList);
     } catch (error) {
         res.status(500).send(error.message);
     }
 }
+
+// #####################################################################################
 
 export async function createChoice(req, res){
     try {
